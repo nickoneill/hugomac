1082440b82f0f1f3f79762cc409c74fd7affd867
Nick O'Neill <nick.oneill@gmail.com>
2015-08-31 11:43:24 -0700

    Initial Commit

---
+++ hugomac/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  hugomac
+//
+//  Created by Nick O'Neill on 8/31/15.
+//  Copyright © 2015 Launch Apps. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ hugomac/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  hugomac
+//
+//  Created by Nick O'Neill on 8/31/15.
+//  Copyright © 2015 Launch Apps. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
